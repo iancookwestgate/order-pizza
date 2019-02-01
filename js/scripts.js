7037c082d1f1e4426cb18d4c7faf5aa3daaa7675
Ian Cook Westgate <iwestgate@hotmail.com>
2019-02-01 09:30:56 -0800

    Include initial JS constructor and prototypes for assigning value to pizza options.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,52 @@
-function User(monies) {
-  this.monies = monies
+function Pizza() {
+  this.toppings = [];
+  this.size = [];
+  this.price = 0;
 }
 
-function PizzaOrder(toppings, size) {
-  this.toppings = toppings;
-  this.size = size;
-}
-
-PizzaOrder.prototype.calculate = function(moneyArr) {
-  function getTotal(total, num) {
-    return total + num;
+Pizza.prototype.topIt = function(topping) {
+  if (topping === "Cheese") {
+    this.toppings = 1;
+  } else if (topping === "Pepperoni") {
+    this.toppings = 2;
+  } else if (topping === "Mushroom") {
+    this.toppings = 3;
+  } else {
+    alert("Don't order a naked pizza!");
   }
-  var total = moneyArr.reduce(getTotal);
-  this.monies = total + this.monies;
 }
+
+Pizza.prototype.sizeIt = function(size) {
+  if (size === "Personal") {
+    this.size = 8;
+  } else if (size === "Medium") {
+    this.size = 12;
+  } else if (size === "Large") {
+    this.size = 16;
+  } else {
+    alert("Please choose a size!")
+  }
+}
+
+
+
+
+// function User(monies) {
+//   this.monies = monies
+// }
+//
+// function PizzaOrder(toppings, size) {
+//   this.toppings = toppings;
+//   this.size = size;
+// }
+//
+// PizzaOrder.prototype.calculate = function(moneyArr) {
+//   function getTotal(total, num) {
+//     return total + num;
+//   }
+//   var total = moneyArr.reduce(getTotal);
+//   this.monies = total + this.monies;
+// }
 
 $(document).ready(function() {
 
